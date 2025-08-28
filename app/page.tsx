e0420be71e18e473edb95fb4e81dec926150c681
RoggerMS <raongta@gmail.com>
2025-08-28 18:32:06 -0500

    chore: finalize Next.js migration

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,3 @@
-import Hero from '@/components/home/Hero';
-import Features from '@/components/home/Features';
-import CTA from '@/components/home/CTA';
-
 export default function HomePage() {
-  return (
-    <div className="min-h-screen">
-      <Hero />
-      <Features />
-      <CTA />
-    </div>
-  );
+  return <main>Home OK</main>;
 }
